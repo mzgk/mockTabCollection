33906ee4330daa38122761c5036c7f6c581d61b2
takeshi mizugaki <takeshi.mizugaki@ast-net.co.jp>
2016-11-18 13:41:53 +0900

    WIP : タブ表示（カスタムセルクラスの追加。tab, web）

---
+++ mockTabBrowser/TabCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TabCollectionViewCell.swift
+//  mockTabBrowser
+//
+//  Created by mzgk on 2016/11/18.
+//  Copyright © 2016年 mzgk. All rights reserved.
+//
+
+import UIKit
+
+class TabCollectionViewCell: UICollectionViewCell {
+
+}
---
+++ mockTabBrowser/WebCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  WebCollectionViewCell.swift
+//  mockTabBrowser
+//
+//  Created by mzgk on 2016/11/18.
+//  Copyright © 2016年 mzgk. All rights reserved.
+//
+
+import UIKit
+
+class WebCollectionViewCell: UICollectionViewCell {
+    
+}
